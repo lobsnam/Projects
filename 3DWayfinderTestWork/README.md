a58f20592a7ca7401c590c265ce6efac95681c12
lobsnam <lobsnam@gmail.com>
2018-05-11 16:10:06 +0530

    Add files via upload

---
+++ 3DWayfinderTestWork/README.md
@@ -0,0 +1,52 @@
+# 3DWayfinderAngular
+Angular Service for 3D Wayfinder (now supports 2D maps also)
+
+## Getting started
+
+### Install
+```bash 
+bower install --save 3dwayfinder-angular
+```
+
+Please include the following JavaScript files into Your HTML file and inject wfangular into Your Angular app:
+
+```javascript
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+<!-- FOR 3D Map add the line below -->
+<script type="text/javascript" src="http://static.3dwayfinder.com/projects/shared/js/minified/frak-stable.min.js"></script>
+<script type="text/javascript" src="http://static.3dwayfinder.com/projects/shared/js/minified/Wayfinder3D.min.js"></script>
+<!-- FOR 2D Map add the line below -->
+<script type="text/javascript" src="http://static.3dwayfinder.com/projects/shared/js/minified/Wayfinder2D.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.6/angular.min.js"></script>
+<script type="text/javascript" src="index.js"></script>
+
+<script>
+    var app = angular.module('basictest', ['wfangular']);
+    app.config(['wfangularConfig', function(wayfinderConfig){
+    	wayfinderConfig.mapType = '2d';
+    }])
+    app.run(['wfangular', function(wayfinder) {
+      wayfinder.open();
+    }]);
+</script>
+```
+## Broadcasted events
+* wf.data.loaded - broadcasted when all data is loaded
+* wf.poi.click - broadcasted when user clicks on map. Arguments: POI
+* wf.language.change - broadcasted when language is changed. Arguments: language code {string} (eg en, de etc)
+* wf.floor.change - broadcasted when floor is changed. Arguments: Floor
+* wf.zoom.change - broadcasted when zoom is changed. Arguments: zoom {float}
+* wf.path.floor.change - broadcasted when path animation changes floor: Arguments: Object {current: Floor, next: Floor, destination: Floor}
+
+## Directives
+* wf-banner - displays a banner, ex. id="advertisements" template="default"
+* wf-floor-buttons - displays floor buttons, on-click function to react, can give additional parameters for the button group via group-class and for the buttons via button-class, see example below
+```javascript
+<wf-floors-buttons group-class="btn-group floors-buttons" active-class="btn btn-positive floor"></wf-floors-buttons>
+```
+
+##Options
+Constant wfangularConfig takes the following arguments for changing the behavior:
+*	assetsLocation: 'http://static.3dwayfinder.com/shared/',
+*	apiLocation: 'http://api.3dwayfinder.com'
+*	mapType: '3d'//2d
---
+++ 3DWayfinderTestWork/index.js
@@ -0,0 +1,322 @@
+/* global $, angular, WayfinderAPI, Wayfinder2D, Wayfinder3D */
+
+var wfangular = angular.module('wfangular', []);
+
+wfangular.constant('wfangularConfig', {
+	assetsLocation: '//static.3dwayfinder.com/shared/',
+	apiLocation: '//api.3dwayfinder.com',
+	mapType: '3d'
+});
+
+
+
+wfangular.factory('wfangular', [
+	'$rootScope',
+	'wfangularConfig',
+	function($rootScope, config) {
+		var wf = false;
+		WayfinderAPI.LOCATION = config.apiLocation;
+
+		if (config.mapType == "3d") {
+			wf = new Wayfinder3D();
+			wf.options.assetsLocation = config.assetsLocation;
+		}
+		else if (config.mapType == "2d")
+			wf = new Wayfinder2D();
+
+
+		if (wf) {
+			wf.cbOnDataLoaded = function() {
+				$rootScope.$broadcast('wf.data.loaded', []);
+			};
+
+			wf.cbOnPOIClick = function(poi) {
+				$rootScope.$broadcast('wf.poi.click', poi);
+			};
+
+			wf.cbOnLanguageChange = function(language) {
+				$rootScope.$broadcast('wf.language.change', language);
+			};
+
+			wf.cbOnFloorChange = function(floor) {
+				$rootScope.$broadcast('wf.floor.change', floor);
+			};
+
+			wf.cbOnZoomChange = function(zoom) {
+				$rootScope.$broadcast('wf.zoom.change', zoom);
+			};
+
+			wf.cbOnBeforeFloorChange = function(currentFloor, nextFloor, destinationFloor) {
+				$rootScope.$broadcast('wf.path.floor.change', {
+					current: currentFloor,
+					next: nextFloor,
+					destination: destinationFloor
+				});
+			};
+
+			wf.cbOnTouch = function(type, value) {
+				$rootScope.$broadcast('wf.touch', {
+					type: type,
+					value: value
+				});
+			};
+
+			wf.cbOnMapReady = function() {
+				$rootScope.$broadcast('wf.map.ready', []);
+			};
+
+			wf.cbOnPathFinished = function(path) {
+				$rootScope.$broadcast('wf.path.finished', path);
+			};
+		}
+
+		return wf;
+	}
+]);
+
+wfangular.filter('wfPOIsByGroupObj', function () {
+	return function (pois, obj) {
+		var filtered = [];
+		if (obj) {
+			angular.forEach(pois, function (item) {
+				if (item.groups && item.groups.indexOf(obj) > -1) {
+					filtered.push(item);
+				}
+			});
+		} else {
+			filtered = pois;
+		}
+		return filtered;
+	}
+});
+
+wfangular.filter('wfGroupsByParentGroupId',function () {
+	return function (groups,num) {
+		var filtered = [];
+		if(num){
+			angular.forEach(groups,function (item) {
+				if(item.parent_id == num){
+					filtered.push(item);
+				}
+			})
+		} else {
+			filtered = groups;
+		}
+		return filtered;
+	}
+});
+
+wfangular.filter('wfPOIsByParentGroupId',function () {
+	return function (pois,num) {
+		var filtered = [];
+		if(num){
+			angular.forEach(pois,function (item) {
+				angular.forEach(item.groups,function (elem) {
+					if(elem.parent_id == num){
+						filtered.push(item);
+					}
+				})
+			})
+		} else {
+			filtered = pois;
+		}
+		return filtered;
+	}
+});
+
+wfangular.directive('wfBanner', ['$interval', 'wfangular', '$timeout', function($interval, wayfinder, $timeout) {
+	return {
+		restrict: 'EA',
+		scope: {},
+		controller: ['$scope', '$element', '$attrs', '$timeout', '$document', function($scope, $element, $attrs, $timeout, $document) {
+			var timeoutId;
+			var frames = [];
+			var current = 0;
+			var timer = null;
+			var id = false;
+			var template = "default";
+
+			//watch id attribute for the banner placement
+			$scope.$watch(function() {
+					return $element.attr('id');
+				},
+				function() {
+					if (!!$attrs.id && $attrs.id !== "") {
+						id = $attrs.id;
+					}
+				}
+			);
+
+			$scope.$watch(function() {
+					return $element.attr('template');
+				},
+				function() {
+					if (!!$attrs.template && $attrs.template !== "") {
+						template = $attrs.template;
+					}
+				}
+			);
+
+			$scope.$on('wf.data.loaded', function(event, data) {
+				setup();
+			});
+
+			function setup() {
+				var tpl = '<div style="position: absolute; background-size: cover; background-position: 50% 50%; background-repeat: no-repeat; left: {1}%; top: {2}%; width: {3}%; height: {4}%; {5}" data-id="{0}"></div>';
+
+				if (wayfinder.advertisements["template-" + template] && wayfinder.advertisements["template-" + template][id]) {
+					frames = wayfinder.advertisements["template-" + template][id];
+				}
+
+				if (frames && frames.length > 0)
+					$document.find("body").addClass("banner-" + id);
+
+				for (var i = 0; i < frames.length; i++) {
+					var frame = frames[i];
+					frame.element = $('<div style="position: relative; width: 100%; height: 100%;">');
+					for (var j = 0; j < frame.containers.length; j++) {
+						var container = frame.containers[j];
+						var image = '';
+						if (container.advertisement_id > 0 && !(container.type && container.type.substr(0, 5) == 'video')) {
+							image = "background-image: url('{0}');".format(WayfinderAPI.advertisements.data.url(container.advertisement_id));
+						}
+						container.element = $(tpl.format(
+							container.id,
+							container.left,
+							container.top,
+							container.width,
+							container.height,
+							image));
+						if (container.advertisement_id > 0 && container.type.substr(0, 5) === 'video') {
+							container.element.append($('<video width="100%" height="auto" src="{0}" loop></video>'.format(
+								WayfinderAPI.advertisements.data.url(container.advertisement_id))));
+						}
+						frame.element.append(container.element);
+					}
+					frame.element.hide();
+				}
+
+				play();
+			}
+
+			function play() {
+				if (current >= frames.length) {
+					return;
+				}
+				var frame = frames[current];
+				frame.element.show();
+				$element.empty().append(frame.element);
+				for (var i = 0; i < frame.containers.length; i++) {
+					if (frame.containers[i].type && frame.containers[i].type.substr(0, 5) === 'video' && frame.containers[i].element) {
+						var el = $(frame.containers[i].element).children('video').get(0);
+						el.play();
+						if (el.readyState) {
+							el.currentTime = '0';
+						}
+					}
+					if (frame.containers[i].element && frame.containers[i].element.hammer) {
+						frame.containers[i].element.on("click", onClick);
+					}
+				}
+				next();
+
+				if (timer) {
+					$timeout.cancel(timer);
+				}
+
+				timer = $timeout(play, frame.duration);
+			}
+
+			function next() {
+				if (current < frames.length - 1) {
+					current++;
+				}
+				else
+					current = 0;
+			}
+
+			function onClick(event) {
+				//var id = $(event.currentTarget).data('id');
+				// console.log('TODO: Banner.onClick', id);
+			}
+		}]
+	}
+}]);
+
+wfangular.directive('wfFloorsButtons', ['wfangular', function(wayfinder) {
+    return {
+        restrict: 'AE',
+        template: '<div class="{{buttonClass}}"' +
+            ' ng-class="{\'{{activeClass}}\': floor.getActive()}"' +
+            ' ng-repeat="floor in data.floors"' +
+            ' ng-click="onFakeClick(floor)">{{floor.getNames() | wfCurrentLanguage}}</div>',
+        scope: {
+            onFakeClick: '&',
+            onClick: '&?',
+            buttonClass: '@',
+            activeClass: '@'
+        },
+        controller: function($scope) {
+            $scope.data = {
+                floors: []
+            };
+
+            $scope.$on('wf.data.loaded', function(event, data) {
+                $scope.data.floors = wayfinder.building.getSortedFloors();
+                console.log("wayfinder.building.getSortedFloors();", wayfinder.building.getSortedFloors())
+            });
+
+            $scope.onFakeClick = function(sa) {
+                if(typeof $scope.onClick === 'function'){
+                    $scope.onClick({
+                        floor: sa
+                    });
+                }
+                else {
+                    console.log("else", sa);
+                    wayfinder.showFloor(sa);
+                }
+            }
+        }
+    }
+}]);
+
+wfangular.directive('wfResize', function($window) {
+	return {
+		restrict: 'AEC',
+		scope: {
+			cbResize: '&cbResize'
+		},
+		link: function(scope, element) {
+
+			var w = element[0];
+			scope.getWindowDimensions = function() {
+				return {
+					'h': w.clientHeight,
+					'w': w.clientWidth
+				};
+			};
+
+			scope.$watch(scope.getWindowDimensions, function(newValue) {
+				scope.windowHeight = newValue.h;
+				scope.windowWidth = newValue.w;
+
+				scope.style = function() {
+					return {
+						'height': (newValue.h - 10) +
+							'px',
+						'width': (newValue.w - 10) +
+							'px'
+					};
+				};
+
+			}, true);
+
+			angular.element($window).bind('resize', function() {
+				scope.$apply(function() {
+					scope.cbResize();
+				});
+			});
+		}
+	}
+});
